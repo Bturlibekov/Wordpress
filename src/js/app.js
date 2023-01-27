73306ac2981f22d788333e09c0bbaa54091bd590
Bekjan <Bekjan>
2023-01-27 21:18:04 +0500

    Начало

---
+++ src/js/app.js
@@ -0,0 +1,77 @@
+// import * as flsFunctions from "./modules/functions.js"
+// import { Modal } from '../js/libs/modal.js'
+// import { Accordion } from '../js/libs/accordion.js'
+// import * as noUiSlider from 'nouislider'
+// import fslightbox from "fslightbox";
+// import { animOnScroll } from "./libs/animOnScroll.js"
+// import { headerFixed } from "./libs/fixed-header.js";
+// import { simpleBar } from "simplebar"; => data-simplebar должен быть у обьекта
+// import { select } from './libs/select.js';
+// import { lazyLoading } from './libs/lazy-loading.js';
+import { getHeaderHeight } from './libs/header-height.js';
+// import { mobileCheck } from './libs/mobile-check.js';
+// import { backgroundImageWebp } from './libs/backgroundImageWebp.js'
+import Swiper, { Navigation, Pagination, Autoplay, EffectFade } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
+// import AOS from 'aos'
+
+// AOS.init()
+
+// flsFunctions.isWebP()
+// animOnScroll()
+// headerFixed();
+// Accordion()
+// select();
+// lazyLoading();
+getHeaderHeight();
+// mobileCheck();
+// backgroundImageWebp()
+
+// Слайдер
+
+const partners = new Swiper('.partners__slider', {
+  loop: true,
+  autoplay: true,
+  speed: 1000,
+  spaceBetween: 15,
+  breakpoints: {
+    375: {
+      slidesPerView: 2,
+    },
+    540: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 6,
+    }
+
+
+  }
+});
+
+// Слайдер
+
+// Бургер меню
+
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+const body = document.body;
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle('burger--active');
+  navbar.classList.toggle('navbar--active');
+  body.classList.toggle('is-lock')
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === navbar) {
+    burger.classList.remove('burger--active');
+    navbar.classList.remove('navbar--active');
+    body.classList.remove('is-lock')
+  }
+})
+
+// Бургер меню
